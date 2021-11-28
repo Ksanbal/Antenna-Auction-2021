b0deb6ca78303d0074e03437a587133130c7a244
Ksanbal <dev.ksanbal@gmail.com>
2021-11-28 21:29:08 +0900

    Feat. firebase List

--- lib/src/view/home.view.dart
+++ lib/src/view/home.view.dart
@@ -1,19 +1,105 @@
-import 'package:antenna_auction_2021/main.dart';
+import 'package:antenna_auction_2021/src/view/regist.view.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
+// ignore: use_key_in_widget_constructors
 class HomeView extends StatelessWidget {
-  const HomeView({Key? key}) : super(key: key);
+  final Stream<QuerySnapshot> _itemStream =
+      FirebaseFirestore.instance.collection('items').snapshots();
+  firebase_storage.FirebaseStorage _storage =
+      firebase_storage.FirebaseStorage.instance;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("안테나의 애장품"),
+        title: const Text("안테나의 애장품"),
       ),
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {},
-        label: Text("추가하기"),
-        icon: Icon(Icons.add),
+        onPressed: () => Get.bottomSheet(const RegistView()),
+        label: const Text("추가하기"),
+        icon: const Icon(Icons.add),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: _itemStream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return const Center(
+              child: Text("Stream builder Error :("),
+            );
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return Padding(
+            padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+            child: GridView.count(
+              crossAxisCount: 2,
+              children: snapshot.data!.docs.map((DocumentSnapshot document) {
+                Map<String, dynamic> data =
+                    document.data()! as Map<String, dynamic>;
+                final ref = _storage.ref().child(data['image']);
+
+                return Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      primary: Colors.white,
+                    ),
+                    child: Column(
+                      children: [
+                        Expanded(
+                          child: FutureBuilder(
+                            future: ref.getDownloadURL(),
+                            builder:
+                                (BuildContext context, AsyncSnapshot snapshot) {
+                              if (snapshot.hasData == false) {
+                                return const Center(
+                                  child: CircularProgressIndicator(),
+                                );
+                              } else {
+                                return Image.network(
+                                  snapshot.data,
+                                  fit: BoxFit.fitWidth,
+                                );
+                              }
+                            },
+                          ),
+                        ),
+                        Text(
+                          data['name'],
+                          style: const TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
+                        Text(
+                          "${data['writer']} 안테나",
+                          style: const TextStyle(
+                            color: Colors.grey,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              }).toList(),
+            ),
+          );
+        },
       ),
     );
   }
