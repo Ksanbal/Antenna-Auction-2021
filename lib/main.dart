b0a0991349f0a5e9577aac7296f2b5a52dadb595
Ksanbal <dev.ksanbal@gmail.com>
2021-11-28 21:28:44 +0900

    Feat. Set main

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:antenna_auction_2021/src/view/home.view.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(App());
+  runApp(const App());
 }
 
 class App extends StatefulWidget {
+  const App({Key? key}) : super(key: key);
+
   // Create the initialization Future outside of `build`:
   @override
   _AppState createState() => _AppState();
@@ -26,23 +29,18 @@ class _AppState extends State<App> {
       builder: (context, snapshot) {
         // Check for errors
         if (snapshot.hasError) {
-          return MaterialApp(
-            theme: ThemeData(
-              primaryColor: Colors.cyan,
-            ),
-            home: Container(
-              child: Center(
-                child: Text("Firebase Error :("),
-              ),
+          return const MaterialApp(
+            home: Center(
+              child: Text("Firebase Error :("),
             ),
           );
         }
 
         // Once complete, show your application
         if (snapshot.connectionState == ConnectionState.done) {
-          return MaterialApp(
+          return GetMaterialApp(
             theme: ThemeData(
-              primaryColor: Colors.cyan,
+              primaryColor: Colors.indigo,
             ),
             home: HomeView(),
           );
@@ -50,8 +48,8 @@ class _AppState extends State<App> {
 
         // Otherwise, show something whilst waiting for initialization to complete
         // return Loading();
-        return Container(
-          child: Center(
+        return const MaterialApp(
+          home: Center(
             child: CircularProgressIndicator(),
           ),
         );
